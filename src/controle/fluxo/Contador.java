cd319b6fc755ad66080427a605f77569095ad48c
Rafael Feitosa <rafaelfeit@hotmail.com>
2025-03-04 14:44:00 -0300

    Criação do desafio DIO sobre controle de fluxo

---
+++ src/controle/fluxo/Contador.java
@@ -0,0 +1,38 @@
+package controle.fluxo;
+
+import java.util.Scanner;
+
+import exception.ParametrosInvalidadosException;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro parâmetro: ");
+        int parametroUm = scanner.nextInt();
+
+        System.out.print("Digite o segundo parâmetro: ");
+        int parametroDois = scanner.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidadosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+        } finally {
+            scanner.close();
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidadosException {
+
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidadosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println(String.format("Imprimindo o nº %d", i));
+        }
+    }
+}
---
+++ src/exception/ParametrosInvalidadosException.java
@@ -0,0 +1,5 @@
+package exception;
+
+public class ParametrosInvalidadosException extends Exception {
+
+}
